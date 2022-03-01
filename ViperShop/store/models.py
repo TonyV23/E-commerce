09f8120fe8642ee08b56de4ff690e483c65be18e
viper23 <htonnycarlos@gmail.com>
2022-03-01 08:57:31 +0200

    added model for product

--- ViperShop/store/models.py
+++ ViperShop/store/models.py
@@ -1,3 +1,13 @@
+from distutils.command.upload import upload
+import black
 from django.db import models
 
 # Create your models here.
+
+class Product (models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField(default=0.0)
+    stock = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    thumbnail = models.ImageField(upload_to="products", blank=True, null=True)
+    
