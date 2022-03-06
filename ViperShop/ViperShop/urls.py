1405232201b431ddb5e72758e4690f095748c26a
viper23 <htonnycarlos@gmail.com>
2022-03-07 00:39:50 +0200

    registration form

--- ViperShop/ViperShop/urls.py
+++ ViperShop/ViperShop/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from store.views import index ,product_detail
+from accounts.views import signup
 from django.conf.urls.static import static
 from ViperShop import settings
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('admin/', admin.site.urls),
+    path('signup/', signup, name='signup'),
+    path('admin/', admin.site.urls),    
     path('product/<str:slug>/', product_detail, name='product'),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
