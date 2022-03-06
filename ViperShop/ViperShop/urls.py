f9665e4e015a5e6f45e0d7162fea7c3924bde708
viper23 <htonnycarlos@gmail.com>
2022-03-06 21:43:08 +0200

    added media & display all produts to home page

--- ViperShop/ViperShop/urls.py
+++ ViperShop/ViperShop/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from store.views import index
+from django.conf.urls.static import static
+
+from ViperShop import settings
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-]
+    
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- ViperShop/store/views.py
+++ ViperShop/store/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from store.models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'store/index.html')
+    products = Product.objects.all() # get all entries of database
+    return render(request, 'store/index.html', context={'products' : products})
