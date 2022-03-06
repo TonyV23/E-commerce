c914661393a0cf8fd21e80f2c44faad889b23a10
viper23 <htonnycarlos@gmail.com>
2022-03-06 21:01:50 +0200

    logic for home page

--- ViperShop/ViperShop/urls.py
+++ ViperShop/ViperShop/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from store.views import index
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
 ]
