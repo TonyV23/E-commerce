d4300fb4aaeee64e2a7eea2898fa1f18820ddd31
viper23 <htonnycarlos@gmail.com>
2022-03-06 23:32:24 +0200

    linked home page to product detail page

--- ViperShop/store/models.py
+++ ViperShop/store/models.py
@@ -1,3 +1,4 @@
+from django.urls  import reverse
 from django.db import models
 
 # Create your models here.
@@ -12,3 +13,6 @@ class Product (models.Model):
 
     def __str__ (self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("product", kwargs= {'slug':self.slug})
