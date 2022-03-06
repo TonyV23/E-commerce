eec5b9f16aaa814e24aafde0da3a9d0dafa72291
viper23 <htonnycarlos@gmail.com>
2022-03-07 01:37:37 +0200

    retreive form information

--- ViperShop/accounts/views.py
+++ ViperShop/accounts/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model,login 
 
+User = get_user_model()
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = User.objects.create_user(username=username, password=password)
+        login(request, user)
+        return redirect('index')
+
     return render(request, 'accounts/signup.html')
