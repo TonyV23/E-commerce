a9f60f4fc7f5cc9b4385bc482799713000e63d24
viper23 <htonnycarlos@gmail.com>
2022-03-08 00:16:37 +0200

    simple changes

--- ViperShop/ViperShop/urls.py
+++ ViperShop/ViperShop/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from store.views import index ,product_detail
-from accounts.views import signup, logout_user,login_user
+from accounts.views import signup, logout_user,login_user,add_to_cart
 from django.conf.urls.static import static
 from ViperShop import settings
 
@@ -13,5 +13,6 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('admin/', admin.site.urls),    
     path('product/<str:slug>/', product_detail, name='product'),
+    path('product/<str:slug>/add-to-cart', add_to_cart, name='add-to-cart'),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
