ba7cee4987b1c238e0351885462addac9de72942
viper23 <htonnycarlos@gmail.com>
2022-03-07 22:58:22 +0200

    ajout de la fonctionalité du panier acheteur

--- ViperShop/store/admin.py
+++ ViperShop/store/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from store.models import Product,Order
+from store.models import Product,Order,Cart
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Cart)
--- ViperShop/store/models.py
+++ ViperShop/store/models.py
@@ -33,6 +33,16 @@ class Order(models.Model):
 
 # Panier (cart)
 """
-    utilisateur, 
+    utilisateur, articles, commandé ou non, date de commande
 """
+
+class Cart(models.Model):
+    user = models.OneToOneField(AUTH_USER_MODEL, on_delete = models.CASCADE)
+    orders = models.ManyToManyField(Order)
+    ordered = models.BooleanField(default=True)
+    ordered_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+        
 
