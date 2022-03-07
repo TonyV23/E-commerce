a73c3010ff21d4fcaa39db52f9856bf897b6755a
viper23 <htonnycarlos@gmail.com>
2022-03-07 22:17:45 +0200

    added feature to connect existing user

--- ViperShop/accounts/views.py
+++ ViperShop/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import get_user_model,login,logout 
+from django.contrib.auth import get_user_model,login,logout,authenticate 
 
 User = get_user_model()
 
@@ -12,6 +12,17 @@ def signup(request):
         return redirect('index')
 
     return render(request, 'accounts/signup.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
+            
+    return render(request, 'accounts/login.html')    
 
 def logout_user(request):
     logout(request)
