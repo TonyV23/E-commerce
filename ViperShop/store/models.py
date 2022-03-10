afd129244c04ccadd75ed997922f3fcc058e523a
viper23 <htonnycarlos@gmail.com>
2022-03-10 06:24:05 +0200

    modify models of orders and carts

--- ViperShop/store/models.py
+++ ViperShop/store/models.py
@@ -27,6 +27,7 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete = models.CASCADE)
     quantity = models.IntegerField(default=1)
     ordered = models.BooleanField(default=False)
+    ordered_date = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.product.name} ({self.quantity})"
@@ -39,9 +40,7 @@ class Order(models.Model):
 class Cart(models.Model):
     user = models.OneToOneField(AUTH_USER_MODEL, on_delete = models.CASCADE)
     orders = models.ManyToManyField(Order)
-    ordered = models.BooleanField(default=True)
-    ordered_date = models.DateTimeField(blank=True, null=True)
-
+      
     def __str__(self):
         return self.user.username
         
